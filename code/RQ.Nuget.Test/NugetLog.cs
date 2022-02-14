8588b347ff7ec95b5527ddb5b7ccf0ac835da662
zrq-github <46354863+zrq-github@users.noreply.github.com>
2022-02-14 14:41:42 +0800

    创建一个分支v1.3.0
    增加一些信息

---
+++ code/RQ.Nuget.Test/NugetLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RQ.Nuget.Test
+{
+    internal class NugetLog
+    {
+        /// <summary>
+        /// Nuget的一些信息
+        /// </summary>
+        /// <returns></returns>
+        public static string IsWho()
+        {
+            string str = $"this is a Nuget.dll, it's version is v1.3.0";
+            return str;
+            //return string.Empty;
+        }
+    }
+}
